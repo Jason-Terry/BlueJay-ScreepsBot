737e1c0eaa910a87556257ab0edaea76e9b1db63
Terry <jason.a.terry@accenture.com>
2020-02-17 10:51:12 -0500

    Bad Commit, don't do it.

---
+++ src/tasks/BuildTask.ts
@@ -0,0 +1,18 @@
+export class WorkerTask {
+    public static run(creep: Creep) {
+        // Task Setup
+        let cargoTotal = _.sum(creep.carry);
+        // If we have room to carry
+        if (cargoTotal > 0) {
+            let target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+            if (target) {
+                if (creep.build(target) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                }
+            }
+        } else {
+            creep.memory.prevTask = creep.memory.currTask;
+            creep.memory.currTask = "RFL";
+        }
+    }
+}
